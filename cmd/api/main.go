710b2ae16e3dc9dff98bf055cef04c2f906baba7
Jon-Martin Storm <jonmartin.storm@gmail.com>
2025-10-07 12:40:34 +0200

    Restart

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,48 +1,18 @@
 package main
 
 import (
-	"encoding/json"
-	"log/slog"
+	"log"
 	"net/http"
-	"os"
 )
 
-func main() {
-	slog.NewJSONHandler(os.Stdout, nil)
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("GET /{$}", home)
-
-	err := http.ListenAndServe(":4001", mux)
-	if err != nil {
-		slog.Error("Failed to start server", "error", err)
-		os.Exit(1)
-	}
-}
-
-type Scenario struct {
-	ID          int      `json:"id"`
-	Tittel      string   `json:"title"`
-	Beskrivelse string   `json:"description"`
-	Temaer      []string `json:"themes"`
-}
-
 func home(w http.ResponseWriter, r *http.Request) {
-	scenarios := []Scenario{
-		{
-			ID:          1,
-			Tittel:      "Feilkonfigurasjon i IAM gir uautorisert tilgang",
-			Beskrivelse: "En ny IAM-policy ble rullet ut uten test. Dette ga tilgang til sensitive data.",
-			Temaer:      []string{"IAM", "Personvern"},
-		},
-		{
-			ID:          2,
-			Tittel:      "Endring i skytjeneste uten rollback",
-			Beskrivelse: "En oppgradering førte til driftsstans uten mulighet for reversering.",
-			Temaer:      []string{"Endringshåndtering", "Drift"},
-		},
-	}
+	w.Write([]byte("Hello fra scenariobanken"))
+}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(scenarios)
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/jonmartinstorm/scenario-banken
 
-go 1.24.4
+go 1.25.1
